2705488454d13d87d731ade64eccb3f0d980930d
Bugan <dones.bugan@gmail.com>
2023-01-18 13:29:41 -0300

    adicinando features

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,21 +1,26 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const placeholderModificada = `${props.placeholder}...`;
+  const erro = true;
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
 
-    const placeholderModificada = `${props.placeholder}...` 
+  return (
+    <div className='campo-texto'>
+      <label>{props.label}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+        id={props.id}
+      />
+      {}
+      {erro ? <p id={`${props.id}-erro`}>Mensagem de erro</p> : ""}
+    </div>
+  );
+};
 
-    const aoDigitado = (evento) => {
-        props.aoAlterado(evento.target.value)
-    }
-
-    return (
-        <div className="campo-texto">
-            <label>
-                {props.label}
-            </label>
-            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
